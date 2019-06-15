8124d03ef75b2805aa3ba5010a1fd16605297636
zhangyingying2017 <18239977435@163.com>
2019-06-15 17:09:49 +0800

    html练习

---
+++ e-life2/e-life/js/about.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+/*$(document).ready(function() {
+$("#zhuce").mouseover(function(){
+		$("#zhuce").animate({
+			color:"#e4e09b",
+			backgroundColor:"rgb(10,24,38)",
+			borderColor:"green"
+			
+			});
+		});
+	$("#zhuce").mouseout(function(){
+		$("#zhuce").animate({
+			color:"#e4e09b",
+			backgroundColor:"rgb(66,82,97)"
+			
+			});
+		});
+});*/
